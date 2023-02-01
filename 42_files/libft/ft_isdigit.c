05dcd3b6d2264deb715a18cc12344448c0574ee3
Gin-Jo <geoffrey.d2002@gmail.com>
2023-02-01 19:57:46 +0100

    adding ft_isdigit

--- 42_files/libft/ft_isdigit.c
+++ 42_files/libft/ft_isdigit.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 17:37:59 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/01 19:53:05 by gdornic          ###   ########.fr       */
+/*   Created: 2023/02/01 19:50:12 by gdornic           #+#    #+#             */
+/*   Updated: 2023/02/01 19:51:59 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-
-#endif
+int	ft_isdigit(int c)
+{
+	if (c <= '9' && c >= '0')
+		return (1);
+	return (0);
+}
