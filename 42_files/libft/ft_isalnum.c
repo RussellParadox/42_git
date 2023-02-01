53f3d9ef72aa471e9df3b2f1dbf2ba920d6d98be
Gin-Jo <geoffrey.d2002@gmail.com>
2023-02-01 20:30:55 +0100

    correction with static

--- 42_files/libft/ft_isalnum.c
+++ 42_files/libft/ft_isalnum.c
@@ -6,18 +6,18 @@
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 20:06:09 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/01 20:24:20 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/02/01 20:30:31 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isdigit(int c)
+static int	ft_isdigit(int c)
 {
 	if (c <= '9' && c >= '0')
 		return (1);
 	return (0);
 }
 
-int	ft_isalpha(int c)
+static int	ft_isalpha(int c)
 {
 	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
 		return (1);
