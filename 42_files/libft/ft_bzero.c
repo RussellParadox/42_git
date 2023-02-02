8fc454e3f8104d2b6ad2bb27bfa8b1be2cb1603c
Gin-Jo <geoffrey.d2002@gmail.com>
2023-02-02 19:03:04 +0100

    modif ft_memset(with no sense) and add ft_bzero

--- 42_files/libft/ft_bzero.c
+++ 42_files/libft/ft_bzero.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 12:12:40 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/02 19:00:30 by gdornic          ###   ########.fr       */
+/*   Created: 2023/02/02 17:50:23 by gdornic           #+#    #+#             */
+/*   Updated: 2023/02/02 19:01:09 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memset(void *s, int c, size_t n)
+static void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*s_toint;
 
@@ -23,4 +23,9 @@ void	*ft_memset(void *s, int c, size_t n)
 		n -= sizeof(char);
 	}
 	return (s);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, '\0', n);
 }
