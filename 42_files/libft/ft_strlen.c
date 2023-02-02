9675abf7538faf590e38fbadfae52553cf324ffd
Gin-Jo <geoffrey.d2002@gmail.com>
2023-02-02 09:49:39 +0100

    adding ft_strlen

--- 42_files/libft/ft_strlen.c
+++ 42_files/libft/ft_strlen.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 17:37:59 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/02 09:44:42 by gdornic          ###   ########.fr       */
+/*   Created: 2023/02/02 09:33:00 by gdornic           #+#    #+#             */
+/*   Updated: 2023/02/02 09:37:16 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
+#include <stddef.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-size_t	ft_strlen(const char *c);
+size_t	ft_strlen(const char *c)
+{
+	int	l;
 
-#endif
+	l = 0;
+	while (c[l])
+		l++;
+	return (l);
+}
