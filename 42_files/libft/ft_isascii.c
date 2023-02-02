dba8ad0cb7b26b4ca44611cfaa7ebd0d27491a28
Gin-Jo <geoffrey.d2002@gmail.com>
2023-02-02 09:32:35 +0100

    adding ft_isascii and ft_isprint

--- 42_files/libft/ft_isascii.c
+++ 42_files/libft/ft_isascii.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 17:37:59 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/02 09:24:11 by gdornic          ###   ########.fr       */
+/*   Created: 2023/02/02 09:00:53 by gdornic           #+#    #+#             */
+/*   Updated: 2023/02/02 09:04:25 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
+int	ft_isascii(int c)
+{
+	if (c <= 127 && c >= 0)
+		return (1);
+	return (0);
+}
--- 42_files/libft/ft_isprint.c
+++ 42_files/libft/ft_isprint.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 17:37:59 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/02 09:24:11 by gdornic          ###   ########.fr       */
+/*   Created: 2023/02/02 09:22:35 by gdornic           #+#    #+#             */
+/*   Updated: 2023/02/02 09:23:54 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	if (c <= '~' && c >= ' ')
+		return (1);
+	return (0);
+}
