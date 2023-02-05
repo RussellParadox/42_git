8a4c0b36cae67044c415290ae68b78c27bedee6f
Geoffrey Dornic <gdornic@c2r3p4.42perpignan.fr>
2023-02-05 14:20:03 +0100

    adding ft_memmove

--- 42_files/libft/libft.h
+++ 42_files/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 17:37:59 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/03 14:00:35 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/02/05 13:17:54 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ size_t	ft_strlen(const char *c);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
