53d5e003a6adca23f56927975c4ede16dd8215bf
Geoffrey Dornic <gdornic@c2r3p8.42perpignan.fr>
2023-02-06 20:08:18 +0100

    modif strlcat

--- 42_files/libft/ft_strlcat.c
+++ 42_files/libft/ft_strlcat.c
@@ -6,17 +6,20 @@
 /*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 17:43:00 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/05 20:38:42 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/02/06 20:06:24 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_min(size_t a, size_t b)
+static size_t	ft_strllen(char *dst, size_t size)
 {
-	if (a < b)
-		return (a);
-	return (b);
+	size_t	len;
+
+	len = 0;
+	while (dst[len] && len < size)
+		len++;
+	return (len);
 }
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
@@ -26,15 +29,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	free_size;
 
 	src_len = ft_strlen(src);
-	dst_len = ft_min(size, ft_strlen(dst));
-	free_size = size - dst_len;
-	if (src_len > free_size)
+	dst_len = ft_strllen(dst, size);
+	if (dst_len != size)
 	{
-		ft_memcpy(dst + dst_len, src, free_size);
-	}
-	else
-	{
-		ft_memcpy(dst + dst_len, src, src_len);
+		free_size = size - dst_len;
+		if (src_len >= free_size)
+		{
+			ft_memcpy(dst + dst_len, src, free_size - 1);
+			dst[dst_len + free_size - 1] = '\0';
+		}
+		else
+		{
+			ft_memcpy(dst + dst_len, src, src_len);
+			dst[dst_len + src_len] = '\0';
+		}
 	}
 	return (src_len + dst_len);
 }
