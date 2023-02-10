3148990088f2032a2f9f3be0741b75e90a29c843
Gin-Jo <geoffrey.d2002@gmail.com>
2023-02-10 23:06:09 +0100

    adding ft_strjoin

--- 42_files/libft/libft.h
+++ 42_files/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 17:37:59 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/09 16:11:24 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/02/10 22:49:54 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
