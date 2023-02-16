ee1581ea1fb8eed387dc0ade4c0dd18b6bdc5f34
Geoffrey Dornic <gdornic@c2r3p4.42perpignan.fr>
2023-02-16 16:26:53 +0100

    adding ft_striteri

--- 42_files/libft/libft.h
+++ 42_files/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 17:37:59 by gdornic           #+#    #+#             */
-/*   Updated: 2023/02/16 14:52:17 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/02/16 15:23:05 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 size_t	ft_strlen(const char *c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
