926cc5d6ef38f8f09586b7381782b5d34717bb3c
Geoffrey Dornic <gdornic@c2r3p4.42perpignan.fr>
2023-02-19 04:59:29 +0100

    adding base

---
+++ 42_files/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 03:55:05 by gdornic           #+#    #+#             */
+/*   Updated: 2023/02/19 03:58:57 by gdornic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE = 1
+#endif
+
+char	*get_next_line(int fd);
+
+#endif
