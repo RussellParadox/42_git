b4c8bcd8aa32ceacc9fac60fe65c61d975301437
tacos_non_fumeur <geoffrey.d2002@gmail.com>
2023-03-05 13:41:18 +0100

    all the essentials functions for ft_printf

--- 42_files/ft_printf/ft_printf.c
+++ 42_files/ft_printf/ft_printf.c
@@ -6,15 +6,15 @@
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 22:36:09 by gdornic           #+#    #+#             */
-/*   Updated: 2023/03/03 19:13:46 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/03/05 13:40:43 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-//print c, return i + 1
+//print at most n characters from str, return i + n
 //return -1 if an error occurs
-size_t	usual_print(char c, size_t i)
+size_t	usual_print(char *str, size_t i, size_t n)
 {
 }
 
@@ -41,8 +41,26 @@ void	flags_manager(_Bool *flags, char c)
 		flags[4] = 1;
 }
 
+//get data in field_width and return the number of characters read
+size_t	ft_atoi_field(char *s, size_t *field_width)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && ft_isdigit(s[i]))
+	{
+		*field_width = *field_width * 10 + s[i] - '0';
+		i++;
+	}
+	return (i);
+}
+
+//print a valid format, return i plus the number of characters read
+//return -1 if an error occurs
+size_t	print_format(_Bool *flags, size_t *field_width, va_list *ap, size_t i)
+
 //search for an allowed format, print it with the associate argument
-//and return the number of characters printed(j) plus i
+//and return the number of characters printed plus i
 //return -1 if an error occurs
 //
 //field_width order
@@ -63,25 +81,17 @@ size_t	special_print(const char *format, va_list *ap, size_t i)
 		flags_manager(flags, format[j]);
 		j++;
 	}
-	while (format[j] && ft_isdigit(format[i]))
-	{
-		field_width[0] = field_width[0] * 10 + format[j] - '0';
-		j++;
-	}
+	j += ft_atoi_field(&format[j], &field_width[0]);
 	while (format[j] && format[j] == '.')
 	{
 		field_width[1] = 1;
 		j++;
 	}
-	while (format[j] && ft_isdigit(format[i]))
-	{
-		field_width[2] = field_width[2] * 10 + format[j] - '0';
-		j++;
-	}
+	j += ft_atoi_field(&format[j], &field_width[2]);
 	if (ft_strchr("cspdiuxX%", format[j]))
-		return (print_with_format(..));
+		return (print_format(flags, field_width, &ap, i));
 	else
-		return (usual_print());
+		return (usual_print(format, i, j);
 }
 
 int	ft_printf(const char *format, ...)
@@ -96,7 +106,7 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%')
 			i = special_print(&format[i], &ap, i);
 		else
-			i = usual_print(format[i], i);
+			i = usual_print(&format[i], i, 1);
 		if (i < 0)
 			return (-1);
 		i++;
