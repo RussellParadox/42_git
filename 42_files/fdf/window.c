6dde996e9fee76bd9c5b2d4ec6be234db4e0e57b
Geoffrey Dornic <gdornic@c2r2p5.42perpignan.fr>
2023-04-15 01:43:02 +0200

    zoom

--- 42_files/fdf/window.c
+++ 42_files/fdf/window.c
@@ -6,7 +6,7 @@
 /*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 04:58:56 by gdornic           #+#    #+#             */
-/*   Updated: 2023/04/15 01:26:24 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/04/15 01:40:42 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@ int	destroy_hook(t_mlx *mlx)
 	return (0);
 }
 
+void	put_map_to_window(t_map *map, t_mlx *mlx, t_settings settings)
+{
+	t_img		img;
+
+	img.img = mlx_new_image(mlx->instance, settings.max.x, settings.max.y);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+		&img.line_length, &img.endian);
+	map_projection(map, &img, settings);
+	mlx_put_image_to_window(mlx->instance, mlx->win, img.img, 0, 0);
+	mlx_destroy_image(mlx->instance, img.img);
+}
+
 int	key_hook(int keycode, t_mlx *mlx)
 {
 	if (keycode == 0xff1b)
@@ -26,9 +38,19 @@ int	key_hook(int keycode, t_mlx *mlx)
 	return (0);
 }
 
-int	mouse_hook(int button, int x, int y, t_mlx *mlx)
+int	mouse_hook(int button, int x, int y, t_param *param)
 {
+	ft_printf("x=%d, y=%d\n", x, y);
 	if (button == 4)
+	{
+		param->settings->scale = param->settings->scale + 0.2;
+		put_map_to_window(param->map, param->mlx, *(param->settings));
+	}
+	if (button == 5)
+	{
+		param->settings->scale = fmax(0., param->settings->scale - 0.2);
+		put_map_to_window(param->map, param->mlx, *(param->settings));
+	}
 	return (0);
 }
 
@@ -44,18 +66,6 @@ void	put_pixel(t_img *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
-}
-
-void	put_map_to_window(t_map *map, t_mlx *mlx, t_settings settings)
-{
-	t_img		img;
-
-	img.img = mlx_new_image(mlx->instance, settings.max.x, settings.max.y);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
-		&img.line_length, &img.endian);
-	map_projection(map, &img, settings);
-	mlx_put_image_to_window(mlx->instance, mlx->win, img.img, 0, 0);
-	mlx_destroy_image(mlx->instance, img.img);
 }
 
 t_mlx	*init_mlx(t_settings *settings)
@@ -111,7 +121,7 @@ int	print_map(t_map *map)
 	hook_param->map = map;
 	put_map_to_window(map, hook_param->mlx, *(hook_param->settings));
 	mlx_key_hook(hook_param->mlx->win, key_hook, hook_param->mlx);
-	mlx_mouse_hook(hook_param->mlx->win, mouse_hook, hook_param->mlx);
+	mlx_mouse_hook(hook_param->mlx->win, mouse_hook, hook_param);
 	mlx_hook(hook_param->mlx->win, 17, 0L, destroy_hook, hook_param->mlx);
 	mlx_loop(hook_param->mlx->instance);
 	free_param(hook_param);
