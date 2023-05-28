3474458f19ef1c68c6682787823d90b92b9f9c01
Geoffrey Dornic <gdornic@c2r2p3.42perpignan.fr>
2023-05-28 17:21:59 +0200

    norm in progress

--- 42_files/fdf/projection.c
+++ 42_files/fdf/projection.c
@@ -6,21 +6,11 @@
 /*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 05:23:00 by gdornic           #+#    #+#             */
-/*   Updated: 2023/05/25 17:34:14 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/05/28 17:20:00 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-t_double3D	cross_product(t_double3D v1, t_double3D v2)
-{
-	t_double3D	result;
-
-	result.x = v1.y * v2.z - v2.y * v1.z;
-	result.y = v1.z * v2.x - v2.z * v1.x;
-	result.z = v1.x * v2.y - v2.x * v1.y;
-	return (result);
-}
 
 double	scalar_product(t_double3D v1, t_double3D v2)
 {
@@ -31,46 +21,59 @@ double	scalar_product(t_double3D v1, t_double3D v2)
 }
 
 //u is the direction vector of the straight line
-t_double2D	parallele_projection(t_int3D coord, t_map *map, t_base3D base, t_double3D u)
+t_double2D	parallele_projection(t_int3D coord, t_map *map, \
+		t_base3D base, t_double3D u)
 {
 }
 
-t_double2D	orthogonal_projection(t_int3D coord, t_map *map, t_base3D base)
+int	projection_color(t_int3D coord, t_map *map)
 {
-	t_double2D	proj;
-	double		ratio;
+	double	ratio;
+	int		color;
 
-	proj.x = (coord.x - map->center.x) * base.e1.x + (coord.y - map->center.y) * base.e2.x + (coord.z - map->center.z) / 10. * base.e3.x;
-	proj.y = (coord.x - map->center.x) * base.e1.y + (coord.y - map->center.y) * base.e2.y + (coord.z - map->center.z) / 10. * base.e3.y;
 	if (map->color_profile)
 	{
 		if (abs(coord.z) < 0.1)
-			proj.color = to_trgb(0, 255, 255, 255);
+			color = to_trgb(0, 255, 255, 255);
 		else if (coord.z >= 0.)
 		{
 			ratio = (double)coord.z / map->apex.z;
-			proj.color = to_trgb(0, 255, 51 + (int)((1. - ratio) * 180), \
+			color = to_trgb(0, 255, 51 + (int)((1. - ratio) * 180), \
 				(int)(ratio * 150));
 		}
 		else
 		{
 			ratio = (double)coord.z / map->abyss.z;
-			proj.color = to_trgb(0, 51, 51 + (int)((1. - ratio) * 100), \
+			color = to_trgb(0, 51, 51 + (int)((1. - ratio) * 100), \
 				100 + (int)(ratio * 150));
 		}
 	}
 	else if (map->color[coord.x][coord.y] == -1)
-		proj.color = to_trgb(0, 255, 255, 255);
+		color = to_trgb(0, 255, 255, 255);
 	else
-		proj.color = map->color[coord.x][coord.y];
+		color = map->color[coord.x][coord.y];
+	return (color);
+}
+
+t_double2D	orthogonal_projection(t_int3D coord, t_map *map, t_base3D base)
+{
+	t_double2D	proj;
+
+	proj.x = (coord.x - map->center.x) * base.e1.x + \
+		(coord.y - map->center.y) * base.e2.x + \
+		(coord.z - map->center.z) / 10. * base.e3.x;
+	proj.y = (coord.x - map->center.x) * base.e1.y + \
+		(coord.y - map->center.y) * base.e2.y + \
+		(coord.z - map->center.z) / 10. * base.e3.y;
+	proj.color = projection_color(coord, map);
 	return (proj);
 }
 
 void	iterative_projection(t_map *map, t_img *img, t_settings settings)
 {
-	register t_int2D	proj_from;
-	register t_int2D	proj_to;
-	register t_int2D		i;
+	t_int2D	proj_from;
+	t_int2D	proj_to;
+	t_int2D	i;
 
 	i.y = 0;
 	while (i.y <= map->max.y)
@@ -78,15 +81,20 @@ void	iterative_projection(t_map *map, t_img *img, t_settings settings)
 		i.x = 0;
 		while (i.x <= map->max.x)
 		{
-			proj_from = scaling(orthogonal_projection((t_int3D){i.x, i.y, map->height[i.x][i.y]}, map, settings.map_base), settings);
+			proj_from = scaling(orthogonal_projection((t_int3D){i.x, \
+			i.y, map->height[i.x][i.y]}, map, settings.map_base), settings);
 			if (i.x < map->max.x)
 			{
-				proj_to = scaling(orthogonal_projection((t_int3D){i.x + 1, i.y, map->height[i.x + 1][i.y]}, map, settings.map_base), settings);
+				proj_to = scaling(orthogonal_projection((t_int3D){i.x + 1, \
+				i.y, map->height[i.x + 1][i.y]}, map, settings.map_base), \
+				settings);
 				bresenham_segment(img, proj_from, proj_to, settings);
 			}
 			if (i.y < map->max.y)
 			{
-				proj_to = scaling(orthogonal_projection((t_int3D){i.x, i.y + 1, map->height[i.x][i.y + 1]}, map, settings.map_base), settings);
+				proj_to = scaling(orthogonal_projection((t_int3D){i.x, \
+				i.y + 1, map->height[i.x][i.y + 1]}, map, settings.map_base), \
+				settings);
 				bresenham_segment(img, proj_from, proj_to, settings);
 			}
 			i.x++;
