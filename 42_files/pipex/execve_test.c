19b3940f2ee618fb4104e9a2039be465138f24ab
Geoffrey Dornic <gdornic@c2r3p3.42perpignan.fr>
2023-08-19 19:43:11 +0200

    bonus in work

--- 42_files/pipex/execve_test.c
+++ 42_files/pipex/execve_test.c
@@ -6,15 +6,24 @@
 /*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 17:40:48 by gdornic           #+#    #+#             */
-/*   Updated: 2023/08/17 18:14:50 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/08/19 14:48:39 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
+void	arr_test(int array[2])
+{
+	array[0] = 2;
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
-	pipeline_fd[0] = open(argv[1], O_RDONLY
-	execve(argv[1], argv + 1, envp);
+	int	array[2];
+
+	array[0] = 0;
+	array[1] = 0;
+	arr_test(array);
+	printf("a[0]: %d\n", array[0]);
 }
--- 42_files/pipex/main.c
+++ 42_files/pipex/main.c
@@ -6,118 +6,157 @@
 /*   By: gdornic <gdornic@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 01:47:06 by gdornic           #+#    #+#             */
-/*   Updated: 2023/08/17 19:04:42 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/08/19 19:38:36 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	child_process(char **cmd, char *envp[], int pipe_fd[2], int pipeline_fd[2])
+int	init_end_fd(int end_fd[2], char *input_end, char *output_end)
 {
-	close(pipe_fd[0]);
-	if (dup2(pipeline_fd[0], 0) == -1)
+	end_fd[0] = open(input_end, O_RDONLY);
+	if (end_fd[0] == -1)
 	{
-		perror("dup2");
-		close(pipe_fd[1]);
+		perror("open");
+		return (1);
+	}
+	end_fd[1] = open(output_end, O_WRONLY);
+	if (end_fd[1] == -1)
+	{
+		perror("open");
+		return (1);
+	}
+	return (0);
+}
+
+int	choose_io_fd(int io_fd[4], int i, int cmd_qt, int end_fd[2])
+{
+	static int	pipe_fd[2];
+
+	if (i == 0)
+	{
+		io_fd[0] = end_fd[0];
+		if (pipe(pipe_fd) == -1)
+		{
+			perror("pipe");
+			return (1);
+		}
+		io_fd[1] = pipe_fd[1];
+		io_fd[2] = 0;
+		io_fd[3] = pipe_fd[0];
+	}
+	else if (i == cmd_qt - 1)
+	{
+		io_fd[0] = pipe_fd[0];
+		io_fd[1] = end_fd[1];
+		io_fd[2] = pipe_fd[1];
+		io_fd[3] = 0;
+	}
+	else
+	{
+		io_fd[0] = pipe_fd[0];
+		io_fd[2] = pipe_fd[1];
+		if (pipe(pipe_fd) == -1)
+		{
+			perror("pipe");
+			return (1);
+		}
+		io_fd[1] = pipe_fd[1];
+		io_fd[3] = pipe_fd[0];
+	}
+	return (0);
+}
+
+int	execute_cmd(char **cmd, int io_fd[3], char *envp[])
+{
+	/*
+	if (io_fd[2] && close(io_fd[2]) == -1)
+	{
+		perror("close");
 		exit(1);
 	}
-	if (dup2(pipe_fd[1], 1) == -1)
+	*/
+	if (io_fd[3] && close(io_fd[3]) == -1)
+	{
+		perror("close");
+		exit(1);
+	}
+	if (dup2(io_fd[0], 0) == -1)
 	{
 		perror("dup2");
-		close(pipe_fd[1]);
+		exit(1);
+	}
+	if (dup2(io_fd[1], 1) == -1)
+	{
+		perror("dup2");
 		exit(1);
 	}
 	if (execve(cmd[0], cmd, envp) == -1)
 	{
 		perror("execve");
-		close(pipe_fd[1]);
 		exit(1);
 	}
-	close(pipe_fd[1]);
 	exit(0);
 }
 
-void	parent_process(char **cmd, char *envp[], int pipe_fd[2], int pipeline_fd[2])
+//io_fd[0]: input fd
+//io_fd[1]: output fd
+//io_fd[2]: pipe input to close
+//io_fd[3]: pipe output to close
+int	pipex(char ***cmd, int end_fd[2], char *envp[], int cmd_qt)
 {
-	close(pipe_fd[1]);
-	if (dup2(pipeline_fd[1], 1) == -1)
+	pid_t	pid;
+	int	io_fd[4];
+	int	status;
+	int	i;
+
+	i = 0;
+	while (i < cmd_qt)
 	{
-		perror("dup2");
-		close(pipe_fd[0]);
-		exit(1);
+		if (choose_io_fd(io_fd, i, cmd_qt, end_fd))
+			return (1);
+		if (io_fd[2] && close(io_fd[2]))
+			return (1);
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			return (1);
+		}
+		else if (pid == 0)
+			execute_cmd(cmd[i], io_fd, envp);
+		else
+		{
+			waitpid(pid, NULL, WNOHANG);
+			if (io_fd[0] && close(io_fd[0]))
+				return (1);
+			if (!WIFEXITED(status) || WEXITSTATUS(status))
+				return (1);
+		}
+		i++;
 	}
-	if (dup2(pipe_fd[0], 0) == -1)
-	{
-		perror("dup2");
-		close(pipe_fd[0]);
-		exit(1);
-	}
-	if (execve(cmd[0], cmd, envp) == -1)
-	{
-		perror("execve");
-		close(pipe_fd[0]);
-		exit(1);
-	}
-	close(pipe_fd[0]);
-	exit(0);
+	return (0);
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	***cmd;
-	pid_t	pid;
-	int		pipeline_fd[2];
-	int		pipe_fd[2];
-	int		status;
-	int		i;
+	int		end_fd[2];
 
 	if (argc < 5)
 		return (EXIT_FAILURE);
-	pipeline_fd[0] = open(argv[1], O_RDONLY);
-	if (pipeline_fd[0] == -1)
-	{
-		perror("open");
+	if (init_end_fd(end_fd, argv[1], argv[argc - 1]))
 		return (EXIT_FAILURE);
-	}
-	pipeline_fd[1] = open(argv[argc - 1], O_WRONLY);
-	if (pipeline_fd[1] == -1)
-	{
-		perror("open");
-		return (EXIT_FAILURE);
-	}
 	cmd = init_cmd(argc, argv);
 	if (cmd == NULL)
 		return (EXIT_FAILURE);
-	print_cmd(cmd);
-	if (pipe(pipe_fd) == - 1)
+	if (pipex(cmd, end_fd, envp, argc - 3))
+		return (EXIT_FAILURE);
+	if (close(end_fd[0]) == -1 || close(end_fd[1]) == -1)
 	{
-		perror("pipe");
+		perror("close");
 		return (EXIT_FAILURE);
 	}
-	pid = fork();
-	i = 0;
-	while (i < argc - 3)
-	{
-		if (pid < 0)
-		{
-			perror("fork");
-			return (EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			child_process(cmd[0], envp, pipe_fd, pipeline_fd);
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-			if (!WIFEXITED(status))
-				return (EXIT_FAILURE);
-			if (!WEXITSTATUS(status))
-				parent_process(cmd[1], envp, pipe_fd, pipeline_fd);
-		}
-	}
-	close(pipeline_fd[0]);
-	close(pipeline_fd[1]);
 	cmd_free(cmd);
 	return (EXIT_SUCCESS);
 }
