25c1329bdbbe45dfd52cc9c2110a032e2e76f8e3
Geoffrey Dornic <gdornic@c2r3p5.42perpignan.fr>
2023-09-19 20:00:31 +0200

    starting philosopher

---
+++ 42_files/philo/philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 19:33:47 by gdornic           #+#    #+#             */
+/*   Updated: 2023/09/19 19:58:56 by gdornic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
